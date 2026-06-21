0875ff25fff9494d64ac61294d08b175df7da6b6
siddheshsdatavista <siddheshs@datavistasolutions.com>
2026-06-21 18:22:09 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
